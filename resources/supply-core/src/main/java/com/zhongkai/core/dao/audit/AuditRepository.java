dbf2d73172443fff600953c5104eafb732300c57
likai1130 <kli@dataman-inc.com>
2018-07-13 18:38:56 +0800

    init code

--- resources/supply-core/src/main/java/com/zhongkai/core/dao/audit/AuditRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zhongkai.core.dao.audit;
-
-import com.zhongkai.core.entity.Audit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuditRepository extends JpaRepository<Audit,Long> {
-}
